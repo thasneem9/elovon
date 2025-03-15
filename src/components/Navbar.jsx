7d9b1b2794598828ed04e19e44ce236bc58e4c59
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-03-15 15:14:42 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
-const Navbar = () => {
+import './navbar.css'
+import logo from '../assets/logo.jpg' 
+function NavBar() {
   return (
-    <div>Navbar</div>
+    <>
+      <img src={logo} alt="logo-elovon" className='nav-logo'/>
+   
+    <nav className='navItems'>
+        <a href="#about">Plans</a>
+        <a href="#trainers" >Trainers</a>
+        <a href="#facilities"  >Facilities</a>
+        <a href="#reviews"  >Reviews</a>
+        <a href="#about-us"  >About us</a>
+    </nav>
+       
+    </>
   )
 }
 
-export default Navbar
+export default NavBar
